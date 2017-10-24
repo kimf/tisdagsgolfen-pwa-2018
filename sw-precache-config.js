4781a4ff52ff32e8cf75fe541fc4d0bbe5cd0392
Kim Fransman <kim.fransman@gmail.com>
2017-10-24 23:01:08 +0200

    add public images to sw-precache

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,10 +2,13 @@ module.exports = {
   stripPrefix: 'build/',
   staticFileGlobs: [
     'build/*.html',
+    'build/*.png',
+    'build/*.gif',
+    'build/*.jpg',
     'build/manifest.json',
     'build/static/**/!(*map*)'
   ],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
   swFilePath: 'build/service-worker.js',
   navigateFallback: 'index.html'
-};
+}
